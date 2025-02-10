eb38b2dc30360be3fcc4ae7c89d861cc6c02fc97
Rbb666 <zhangbingru@rt-thread.com>
2025-02-10 16:23:14 +0800

    删除无用的文件

--- projects/art_pi2_bootloader/applications/main.c
+++ projects/art_pi2_bootloader/applications/main.c
@@ -344,13 +344,6 @@ int main(void)
 
     rt_kprintf("\nJump to APP...\n");
     rt_thread_mdelay(300);
-//       while (1)
-//       {
-//           rt_pin_write(LED_RED, PIN_HIGH);
-//           rt_thread_mdelay(500);
-//           rt_pin_write(LED_RED, PIN_LOW);
-//           rt_thread_mdelay(500);
-//       }
 
     JumpToApplication();
 
